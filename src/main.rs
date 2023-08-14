efc01d2ef52f8db99c7aff9e0f9df8e6a5b010cb
Douglas Onofre <douglas12psp@gmail.com>
2023-08-14 18:27:48 +0100

    Reorganize files to chapter 3

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,15 @@
-use rltk::{GameState, Rltk, VirtualKeyCode, RGB};
+use rltk::{GameState, Rltk, RGB};
 use specs::prelude::*;
 use specs_derive::Component;
-use std::cmp::{max, min};
 
-#[derive(Component)]
-struct Position {
-    x: i32,
-    y: i32,
-}
-
-#[derive(Component)]
-struct Renderable {
-    glyph: rltk::FontCharType,
-    foreground: RGB,
-    background: RGB,
-}
+mod components;
+pub use components::*;
+mod map;
+pub use map::*;
+mod player;
+use player::*;
+mod rect;
+pub use rect::*;
 
 #[derive(Component)]
 struct LeftMover {}
@@ -33,17 +28,14 @@ impl<'a> System<'a> for LeftWalker {
     }
 }
 
-#[derive(Component, Debug)]
-struct Player {}
-
 #[derive(PartialEq, Copy, Clone)]
 enum TileType {
     Wall,
     Floor,
 }
 
-struct State {
-    ecs: World,
+pub struct State {
+    pub ecs: World,
 }
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
@@ -116,33 +108,6 @@ fn main() -> rltk::BError {
     }
 
     rltk::main_loop(context, gs)
-}
-
-fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
-    let mut positions = ecs.write_storage::<Position>();
-    let mut players = ecs.write_storage::<Player>();
-    let map = ecs.fetch::<Vec<TileType>>();
-
-    for (_player, pos) in (&mut players, &mut positions).join() {
-        let destination_idx = xy_idx(pos.x + delta_x, pos.y + delta_y);
-        if (map[destination_idx]) != TileType::Wall {
-            pos.x = min(79, max(0, pos.x + delta_x));
-            pos.y = min(49, max(0, pos.y + delta_y));
-        }
-    }
-}
-
-fn player_input(gs: &mut State, ctx: &mut Rltk) {
-    match ctx.key {
-        None => {}
-        Some(key) => match key {
-            VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
-            VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
-            VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
-            VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
-            _ => {}
-        },
-    }
 }
 
 pub fn xy_idx(x: i32, y: i32) -> usize {
